1040e3bafad22ab873e797cd60cc34ea83f2eee2
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-12 23:47:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+const newHeader = document.body.appendChild(document.createElement("h1"));
+newHeader.setAttribute("id", "victory");
+
+let nam = "ken";
+newHeader.innerHTML= `${nam} is the champion`;
