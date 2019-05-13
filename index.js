9b353703d7eaa7ac33e07d8fb333503be2b3857c
Matthew Lindsey <mbl@null.net>
2019-05-13 06:30:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "<h1>Your greatest rival is the champion!</h1>"
+newHeader.className = "victory"
